07ea6a4925aec2c169c770660aeeacb8beef9622
PreenaM <pmaruthavel7424@student.palomar.edu>
2024-05-22 22:15:08 -0700

    switch to Game and Screen classes

--- core/src/com/mygdx/game/FarmingSimulator.java
+++ core/src/com/mygdx/game/FarmingSimulator.java
@@ -1,11 +1,11 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class FarmingSimulator extends ApplicationAdapter {
+public class FarmingSimulator extends Game {
 	SpriteBatch batch;
 	Texture img;
 	
@@ -13,14 +13,12 @@ public class FarmingSimulator extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		this.setScreen(new FarmScreen());
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
 	
 	@Override
